67a9a2a2763629223823ba264a124affdae5029d
Chukwukaelo Osuizugbe <42609625+mail2chuka@users.noreply.github.com>
2024-07-24 20:20:01 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -66,7 +66,7 @@ function Footer() {
       </div>
       <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-100 lg:my-8" />
       <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://ecanagroup.com/" className="hover:underline">ECANA Group™</a>. All Rights Reserved.
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://ecanagroup.com/" className="hover:underline">ECANA Group™</a>. All Rights Reserved.
           </span>
           <div className="flex mt-4 sm:justify-center sm:mt-0">
              
