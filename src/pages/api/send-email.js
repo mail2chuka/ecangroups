468d93ce32c5185035e94c04eb787e29e7564041
Chukwukaelo Osuizugbe <mail2chuka@gmail.com>
2025-08-31 11:32:05 +0200

    Update send-email.js

--- src/pages/api/send-email.js
+++ src/pages/api/send-email.js
@@ -37,7 +37,7 @@ const handler = async (req, res) => {
 
     try {
       const { data, error } = await resend.emails.send({
-        from: 'Ecana Group Info <info@ecanagroup.com>', // Update to your verified domain
+        from: 'Ecana-Group Info <info@ecanagroup.com>', // Update to your verified domain
         to: ['info@ecanagroup.com', email], // Your company's email
         subject: subject || 'Contact Form Submission',
         react: EmailTemplate({ firstName, lastName, email, subject, message }),
